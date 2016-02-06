a85e0cdb9465b4a3f5c947e32a9f2a91fa8216ff
aikasunga <aikasunga@gmail.com>
2016-02-06 13:47:09 +0800

    Update ViewCalendar.java

--- ui/ViewCalendar.java
+++ ui/ViewCalendar.java
@@ -12,30 +12,5 @@ public void showCalendar()
 {
 	
 }
-public void showActivityDetails()
-{
-	
-}
-public void addActivity()
-{
-	
-}
-public void enableEditing()
-{
-	
-}
-public void saveChanges()
-{
-	
-}
-public void cancelChanges()
-{
-	
-}
-
-public void disableEditing()
-{
-	
-}
 
 }
