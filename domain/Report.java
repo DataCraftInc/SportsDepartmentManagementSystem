95d8f11bf6aee556ddcd463717095c5b08d3681a
Jayvee Villa <jayvee.villa39@gmail.com>
2016-02-06 12:43:46 +0800

    Create Report.java

---
+++ domain/Report.java
@@ -0,0 +1,13 @@
+package domain;
+
+public class Report 
+{
+	private Activity activity;
+
+	public Activity getActivity() {
+		return activity;
+	}
+	public void setActivity(Activity activity) {
+		this.activity = activity;
+	}
+}
