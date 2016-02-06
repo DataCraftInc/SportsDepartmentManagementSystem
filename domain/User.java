5017d9ec475db346f8e0f5b1e22e0a34ccb80f87
Jayvee Villa <jayvee.villa39@gmail.com>
2016-02-06 12:44:06 +0800

    Create User.java

---
+++ domain/User.java
@@ -0,0 +1,92 @@
+package domain;
+
+import java.util.List;
+
+public class User 
+{
+	private String username;
+	private String password;
+	private List<Module> moduleList;
+	private String lastName;
+	private String firstName;
+	private String gender;
+	private String birthDate;
+	private String address;
+	private String contactNumber;
+	private String email;
+	private String facebook;
+	private String stamp;
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public List<Module> getModuleList() {
+		return moduleList;
+	}
+	public void setModuleList(List<Module> moduleList) {
+		this.moduleList = moduleList;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getBirthDate() {
+		return birthDate;
+	}
+	public void setBirthDate(String birthDate) {
+		this.birthDate = birthDate;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getContactNumber() {
+		return contactNumber;
+	}
+	public void setContactNumber(String contactNumber) {
+		this.contactNumber = contactNumber;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getFacebook() {
+		return facebook;
+	}
+	public void setFacebook(String facebook) {
+		this.facebook = facebook;
+	}
+	public String getStamp() {
+		return stamp;
+	}
+	public void setStamp(String stamp) {
+		this.stamp = stamp;
+	}
+}
