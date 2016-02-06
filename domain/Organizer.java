7bb5cd0973610997f7987896e4ce2823ea98a6cf
Jayvee Villa <jayvee.villa39@gmail.com>
2016-02-06 12:43:27 +0800

    Create Organizer.java

---
+++ domain/Organizer.java
@@ -0,0 +1,48 @@
+package domain;
+
+public class Organizer 
+{
+	private int orgNo;
+	private String lastName;
+	private String firstName;
+	private int age;
+	private String sports;
+	private String stamp;
+	
+	public int getOrgNo() {
+		return orgNo;
+	}
+	public void setOrgNo(int orgNo) {
+		this.orgNo = orgNo;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getSports() {
+		return sports;
+	}
+	public void setSports(String sports) {
+		this.sports = sports;
+	}
+	public String getStamp() {
+		return stamp;
+	}
+	public void setStamp(String stamp) {
+		this.stamp = stamp;
+	}
+}
